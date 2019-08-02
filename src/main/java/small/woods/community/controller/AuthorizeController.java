36074d901245900e80b80103c81d9d2e006890a9
small-woods <1968678010@qq.com>
2019-08-02 11:24:10 +0800

    extract github config from code to properties

---
+++ src/main/java/small/woods/community/controller/AuthorizeController.java
@@ -0,0 +1,42 @@
+package small.woods.community.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import small.woods.community.dto.AccessTokenDTO;
+import small.woods.community.dto.GithubUser;
+import small.woods.community.provider.GithubProvider;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url(redirectUrl);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_secret(clientSecret);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
+
---
+++ src/main/java/small/woods/community/provider/GithubProvider.java
@@ -0,0 +1,48 @@
+package small.woods.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+import small.woods.community.dto.AccessTokenDTO;
+import small.woods.community.dto.GithubUser;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    //使用okhttp发送post请求
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType =MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
+        Request request = new Request
+                .Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+       try (Response response = client.newCall(request).execute()){
+           String string =response.body().string();
+           String token = string.split("&")[0].split("=")[1];
+           System.out.println(token);
+           return token;
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+        return null;
+    }
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string =  response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
