7c80f594e6498890d52622e7e92384954ebae823
small-woods <1968678010@qq.com>
2019-08-03 14:36:55 +0800

    add data to  h2

--- src/main/java/small/woods/community/controller/AuthorizeController.java
+++ src/main/java/small/woods/community/controller/AuthorizeController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import small.woods.community.dto.AccessTokenDTO;
 import small.woods.community.dto.GithubUser;
+import small.woods.community.mapper.UserMapper;
+import small.woods.community.model.User;
 import small.woods.community.provider.GithubProvider;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -22,9 +27,14 @@ public class AuthorizeController {
 
     @Value("${github.redirect.url}")
     private String redirectUrl;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -33,10 +43,25 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         accessTokenDTO.setClient_secret(clientSecret);
 
+
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser !=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功，写cookie 和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
+//        return "index";
     }
 }
 
---
+++ src/main/java/small/woods/community/model/User.java
@@ -0,0 +1,69 @@
+package small.woods.community.model;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String accountId;
+    private String token;
+    private Long gmtCreate;
+    private Long gmtModified;
+
+    public User(){}
+
+    public User(Integer id, String name, String accountId, String token, Long gmtCreate, Long gmtModified) {
+        this.id = id;
+        this.name = name;
+        this.accountId = accountId;
+        this.token = token;
+        this.gmtCreate = gmtCreate;
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
