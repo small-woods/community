09edc2e95dbbd3502352f18703add55bab2b5ed3
small-woods <1968678010@qq.com>
2019-08-04 18:29:38 +0800

    add V3 table to H2

--- src/main/resources/db/migration/V3__Create_question_table.sql
+++ src/main/resources/db/migration/V3__Create_question_table.sql
@@ -1,6 +1,6 @@
-create table quest
+create table question
 (
-	id int auto_increment,
+	id int auto_increment primary key,
 	title varchar(50),
 	description text,
 	gmt_create bigint,
@@ -10,6 +10,4 @@ create table quest
 	view_count int default 0,
 	like_count int default 0,
 	tag varchar(256),
-	constraint quest_pk
-		primary key (id)
 );
