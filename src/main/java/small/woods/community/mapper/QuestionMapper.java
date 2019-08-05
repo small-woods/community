cfc3c3022f5208565d758e5b629eacaa723a48d1
small-woods <1968678010@qq.com>
2019-08-05 13:14:03 +0800

    finish the publish

---
+++ src/main/java/small/woods/community/mapper/QuestionMapper.java
@@ -0,0 +1,12 @@
+package small.woods.community.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import small.woods.community.model.Question;
+
+@Mapper
+public interface QuestionMapper {
+
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+     void create(Question question);
+}
